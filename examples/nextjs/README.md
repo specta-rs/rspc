2874241f81f5f9c23bfab8107feaa81e9bed545c
Marius Jørgensen <mariusjorg93@gmail.com>
2022-09-11 15:42:18 +0200

    Add nextjs example

---
+++ examples/nextjs/README.md
@@ -0,0 +1,19 @@
+<h1 align="center">rspc - Example with Next.js</h1>
+<div align="center">
+ <strong>
+   This example shows basic usage with Next.js and `@tanstack/react-query`.
+ </strong>
+</div>
+
+<br />
+
+- `pages/__app.tsx` - Adds global provider
+- `pages/using-ssp.tsx` - Shows how to execuse queries server side
+- `pages/using-use-mutation.tsx` - Shows how to use `useMutation` hook
+- `pages/using-use-query.tsx` - Shows how to use `useQuery` hook
+- `pages/using-use-subscription.tsx` - Shows how to use `useSubscription` hook
+- `src/rspc.ts` - Shows how to set up RSPC client and hooks
+
+## Getting started
+1. Run `pnpm i` in the repository root folder
+2. Start example projects with `pnpm examples dev`
---
+++ examples/nextjs/pages/index.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => (
+  <div className={styles.container}>
+    <Head>
+      <title>RSPC Example with Next.js</title>
+    </Head>
+
+    <main className={styles.main}>
+      <h1 className={styles.title}>
+        Welcome to <a href="https://rspc.otbeaumont.me" target="_blank">RSPC</a> with <a href="https://nextjs.org" target="_blank">Next.js!</a>
+      </h1>
+
+      <div className={styles.grid}>
+        <Link href="/using-use-query" passHref>
+          <a className={styles.card}>
+            <h2>Using useQuery &rarr;</h2>
+          </a>
+        </Link>
+
+        <Link href="/using-use-mutation" passHref>
+          <a className={styles.card}>
+            <h2>Using useMutation &rarr;</h2>
+          </a>
+        </Link>
+
+        <Link href="/using-use-subscription" passHref>
+          <a className={styles.card}>
+            <h2>Using useSubscription &rarr;</h2>
+          </a>
+        </Link>
+
+        <Link href="/using-ssp" passHref>
+          <a className={styles.card}>
+            <h2>Using ServerSideProps &rarr;</h2>
+          </a>
+        </Link>
+      </div>
+    </main>
+  </div>
+)
+
+export default Home
