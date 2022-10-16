c222aa3493e56be9ae7b319b3d7b257aa356fd92
kkharji <kkharji@protonmail.com>
2022-10-16 16:34:23 +0300

    fix(httpz): update api
    see https://github.com/oscartbeaumont/rspc/commit/e11e19d83077b8d139073bd9541f2f7c01334a10

--- src/integrations/mod.rs
+++ src/integrations/mod.rs
@@ -8,5 +8,15 @@
 ))]
 pub mod httpz;
 
+#[cfg(any(
+    feature = "httpz",
+    feature = "axum",
+    feature = "actix-web",
+    feature = "rocket",
+    feature = "lambda",
+    feature = "workers"
+))]
+pub(crate) mod httpz_extractors;
+
 #[cfg(feature = "tauri")]
 pub mod tauri;
