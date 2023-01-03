e397f238286624183e2c57af05e1994b6a287928
Oscar Beaumont <oscar@otbeaumont.me>
2023-01-03 13:46:56 +0800

    fix issue importing `rollup-preset-solid`

--- packages/solid/rollup.config.js
+++
@@ -1,5 +0,0 @@
-const withSolid = require("rollup-preset-solid").default;
-
-module.exports = withSolid({
-  targets: ["esm", "cjs"],
-});
---
+++ packages/solid/rollup.config.mjs
@@ -0,0 +1,5 @@
+import withSolid from "rollup-preset-solid";
+
+export default withSolid({
+  targets: ["esm", "cjs"],
+});
