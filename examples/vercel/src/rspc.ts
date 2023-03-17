f4d6326e7adf1a4404e73259648630c6a4292f51
Oscar Beaumont <oscar@otbeaumont.me>
2023-03-17 16:43:32 +0800

    bogus commit

--- examples/vercel/src/rspc.ts
+++ examples/vercel/src/rspc.ts
@@ -3,13 +3,12 @@ import { createReactQueryHooks } from "@rspc/react";
 import { QueryClient } from "@tanstack/react-query";
 import type { Procedures } from "./bindings";
 
-const PATH = 'api/rspc';
-const HOST = typeof window === "undefined"
-	? process.env.VERCEL_URL
-	: window.location.host;
+const PATH = "api/rspc";
+const HOST =
+  typeof window === "undefined" ? process.env.VERCEL_URL : window.location.host;
 
 export const client = createClient<Procedures>({
-  transport: new FetchTransport(`http://${HOST}/${PATH}`)
+  transport: new FetchTransport(`http://${HOST}/${PATH}`),
 });
 
 export const queryClient = new QueryClient({
