cbb9080d51c3f1728fbbf769cf89e98eb5b6cb7a
Oscar Beaumont <oscar@otbeaumont.me>
2023-07-22 11:52:46 +0800

    move cra + axum and core crates

---
+++ crates/axum/src/lib.rs
@@ -0,0 +1,20 @@
+//! Axum integration for rspc
+
+// TODO: Crate lints
+
+use std::{any::Any, sync::Arc};
+
+use axum::{body::HttpBody, routing::any};
+use rspc::{BuiltRouter, Router};
+
+/// TODO
+pub fn endpoint<S, B, TCtx: Send + Sync + 'static>(
+    router: Arc<BuiltRouter<TCtx>>,
+    ctx_fn: impl Any,
+) -> axum::Router<S, B>
+where
+    B: HttpBody + Send + 'static,
+    S: Clone + Send + Sync + 'static,
+{
+    axum::Router::new().route("/:id", any(|| async move { "Hello, world!" }))
+}
