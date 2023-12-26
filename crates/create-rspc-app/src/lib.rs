6ca35be19615d19c51b71021c7b8b21d57dd02bf
Oscar Beaumont <oscar@otbeaumont.me>
2023-12-26 23:39:18 +0800

    Drop `crate-rspc-app` for now

--- crates/create-rspc-app/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub(crate) mod database;
-pub(crate) mod framework;
-pub(crate) mod frontend_framework;
-pub(crate) mod generator;
-pub mod internal;
-mod utils;
--- crates/create-rspc-app/templates/tauri_base/README.md
+++
@@ -1,9 +0,0 @@
-# Create rspc app
-
-This app was scaffolded using the [create-rspc-app](https://rspc.dev) CLI.
-
-## Usage
-
-```bash
-cargo tauri dev
-```
