cc14f9f5625f8d5649bf099419bc3ab5ef2c0450
Oscar Beaumont <oscar@otbeaumont.me>
2024-11-21 00:08:57 +0800

    Change feature from `docrs2` back to `docrs`

--- crates/tauri/src/lib.rs
+++ crates/tauri/src/lib.rs
@@ -1,5 +1,5 @@
 //! rspc-tauri: Tauri integration for [rspc](https://rspc.dev).
-#![cfg_attr(docsrs2, feature(doc_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc(
     html_logo_url = "https://github.com/oscartbeaumont/rspc/raw/main/docs/public/logo.png",
     html_favicon_url = "https://github.com/oscartbeaumont/rspc/raw/main/docs/public/logo.png"
