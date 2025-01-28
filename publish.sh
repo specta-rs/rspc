09fd59d9317879559cdd8b72b6d4290a242cc74a
Oscar Beaumont <oscar@otbeaumont.me>
2025-01-28 16:29:28 +0800

    :tada: release 0.4.0

---
+++ publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# TODO: Replace this with some proper tool
+# TODO: Detect if the package has changed and a release is required
+
+set -e
+
+cd crates/core/
+cargo publish
+cd ../../
+
+cd crates/procedure/
+cargo publish
+cd ../../
+
+cd crates/legacy/
+cargo publish
+cd ../../
+
+cd rspc/
+cargo publish
+cd ..
